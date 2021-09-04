d0ea06db1edf559d5a6efe522b93af9b28579c6e
Marco Preciado <mac46005@gmail.com>
2021-09-04 16:54:02 -0500

    Continue on startup

---
+++ MyWebsiteResume/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyWebsiteResume.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyWebsiteResume.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            ViewBag.Name = "Marco";
+            ViewBag.FV = 9999.99;
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(FutureValueModel model)
+        {
+            ViewBag.FV = model.CalculateFutureValue();
+            return View(model);
+        }
+    }
+}
